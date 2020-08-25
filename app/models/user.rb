0543dd704b87b4d5b154aaf570d9cff423f8aabc
satozaki-n <hornon.sato31@gmail.com>
2020-08-25 19:31:57 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true
   validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
 end
