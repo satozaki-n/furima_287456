8b21537716b4223849a47bcaf50592ae9fd25006
satozaki-n <hornon.sato31@gmail.com>
2020-08-27 23:26:30 +0900

    トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'items/index'
   devise_for :users
+  root to: "items#index"
   resources :users, only: [:new, :create]
   get 'users/user_sign_up', to: 'users#new'
 end
