bd69018e0e7fbb90d9ccd7959f9e4707cbb7a212
satozaki-n <hornon.sato31@gmail.com>
2020-08-31 15:55:01 +0900

    発送までの日数プルダウンの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,14 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   #空の投稿を保存できないようにする
-  validates :name, :category, :sales_status, :shipping_fee_status, :prefecture, presence: true
+  validates :name, :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, presence: true
 
   #カテゴリーの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 }
   validates :sales_status_id, numericality: { other_than: 1 }
   validates :shipping_fee_status_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
+  validates :scheduled_delivery_id, numericality: { other_than: 1 }
 
   #アソシエーション
   belongs_to :user
@@ -17,4 +18,5 @@ class Item < ApplicationRecord
   belongs_to_active_hash :sales_status
   belongs_to_active_hash :shipping_fee_status
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
 end
--- db/migrate/20200827133013_create_items.rb
+++ db/migrate/20200827133013_create_items.rb
@@ -8,7 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer   :sales_status_id,       null: false
       t.integer   :shipping_fee_status_id,null: false
       t.integer   :prefecture_id,         null: false
-      t.integer   :scheduled_delivery,    null: false
+      t.integer   :scheduled_delivery_id, null: false
       t.integer   :prise,                 null: false
       t.references  :user,                null: false, foreign_key: true
       t.timestamps
