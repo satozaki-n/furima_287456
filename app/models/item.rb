4b730ecc7581dc08b41e114424c876c999417d2c
satozaki-n <hornon.sato31@gmail.com>
2020-08-31 14:10:04 +0900

    配送料の負担プルダウンの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,17 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   #空の投稿を保存できないようにする
-  validates :name, :category, :sales_status, presence: true
+  validates :name, :category, :sales_status, :shipping_fee_status, presence: true
 
   #カテゴリーの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 }
   validates :sales_status_id, numericality: { other_than: 1 }
+  validates :shipping_fee_status_id, numericality: { other_than: 1 }
 
   #アソシエーション
   belongs_to :user
   has_one_attached :image
   belongs_to_active_hash :category
   belongs_to_active_hash :sales_status
-
+  belongs_to_active_hash :shipping_fee_status
 end
--- db/migrate/20200827133013_create_items.rb
+++ db/migrate/20200827133013_create_items.rb
@@ -1,16 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
-    create_table :items do |t|
-      t.string    :image,                null: false
-      t.string    :name,                 null: false
-      t.text      :info,                 null: false
-      t.integer   :category_id,          null: false
-      t.integer   :sales_status_id,      null: false
-      t.integer   :shipping_fee_status,  null: false
-      t.integer   :prefecture,           null: false
-      t.integer   :scheduled_delivery,   null: false
-      t.integer   :prise,                null: false
-      t.references  :user,               null: false, foreign_key: true
+    create_table :items do |t|  
+      t.string    :image,                 null: false
+      t.string    :name,                  null: false
+      t.text      :info,                  null: false
+      t.integer   :category_id,           null: false
+      t.integer   :sales_status_id,       null: false
+      t.integer   :shipping_fee_status_id,null: false
+      t.integer   :prefecture,            null: false
+      t.integer   :scheduled_delivery,    null: false
+      t.integer   :prise,                 null: false
+      t.references  :user,                null: false, foreign_key: true
       t.timestamps
     end
   end
