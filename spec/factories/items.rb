c5fbe6a0c0aba2a44ddf4f1056eacc03b4b48571
satozaki-n <hornon.sato31@gmail.com>
2020-09-04 22:16:15 +0900

    テストコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :item do
+    image               {}
     name                { Faker::Name }
     info                { Faker::Lorem.sentence }
-    category            { 2 }
-    sales_status        { 2 }
-    shipping_fee_status { 2 }
-    prefecture          { 2 }
-    scheduled_delivery  { 2 }
+    category            { '2' }
+    #sales_status        { '2' }
+    #shipping_fee_status { '2' }
+    #prefecture          { '2' }
+    #scheduled_delivery  { '2' }
     price               { '500' }
     association :user
   end
--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "Items", type: :request do
 
   describe "GET /index" do
-    it "returns http success" do
-      get "/items/index"
+    it 'returns http success' do
+      get '/items/index'
       expect(response).to have_http_status(:success)
     end
   end
