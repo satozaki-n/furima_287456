cd98abc10a1835553c1fb7e3f7ffe7e06fb4dbc6
satozaki-n <hornon.sato31@gmail.com>
2020-09-07 11:24:44 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  
-  #user
-  devise_for :users
-  resources :users, only: [:new, :create, :destroy]
-  get 'users/user_sign_up', to: 'users#new'
 
-  #item
+  devise_for :users
+  resources :users, only: [:new, :create]
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show]
-  get 'items/index'
-  get 'items/new_item', to: 'items#new'
-  post 'items/new', to: 'items#create'
+  resources :items, only: [:index, :new, :create]
 
 
 end
