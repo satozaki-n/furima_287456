9357ba3fd6e9d02a0791f60a8b2d426f82e6114d
satozaki-n <hornon.sato31@gmail.com>
2020-09-15 20:06:31 +0900

    商品出品機能の追加実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,11 +11,11 @@ class ItemsController < ApplicationController
 
   def create
     @items = Item.new(item_params)
-      if @items.save
-        redirect_to root_path
-      else
-        render :new
-      end
+    if @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 
   def show_item
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,14 +5,14 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :info
-    validates :category
-    validates :sales_status
-    validates :shipping_fee_status
-    validates :prefecture
-    validates :scheduled_delivery
-    validates :price, format: { with: /\A^[0-9]+^\z/, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price, numericality: { with: /\A^[0-9]+^\z/ }
   end
-
+  validates_inclusion_of :price,  in: 300..9_999_999
   #プルダウンの選択が「--」の時は保存できないようにする
   with_options numericality: { other_than: 1 } do
     validates :category_id
