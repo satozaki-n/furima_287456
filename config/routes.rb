36cff19c82ecd7fc0d2affca311793ccfe10c993
satozaki-n <hornon.sato31@gmail.com>
2020-09-16 20:37:40 +0900

    詳細表示のルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:new, :create]
   root to: "items#index"
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show]
  
 end
