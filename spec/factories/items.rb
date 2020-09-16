75997f46adf0ab5d8af7fd5e7d14659bb3848fb0
satozaki-n <hornon.sato31@gmail.com>
2020-09-17 00:46:27 +0900

    テストコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     name                { Faker::Name }
     info                { Faker::Lorem.sentence }
-    category            { 2 }
-    sales_status        { '2' }
-    shipping_fee_status { '2' }
-    prefecture          { '2' }
-    scheduled_delivery  { '2' }
+    category_id            { '2' }
+    sales_status_id        { '2' }
+    shipping_fee_status_id { '2' }
+    prefecture_id          { '2' }
+    scheduled_delivery_id  { '2' }
     price               { '500' }
     association :user
   end
