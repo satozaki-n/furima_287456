81fe507de961ba28d6f56c0cadcdc80424d4e40c
satozaki-n <hornon.sato31@gmail.com>
2020-09-16 22:20:59 +0900

    商品の編集機能と削除機能のルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,22 @@ class ItemsController < ApplicationController
 
   def show
     @items = Item.find(params[:id])
-    #@user = current_user.user
+    @user = current_user.nickname
+  end
+
+  def edit
+    @items = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+  def destroy
+    @items = Item.find(params[:id])
+    @items.destroy
+    redirect_to root_path
   end
 
   private
