7436c4fd8308fb0e9b3f083a2854109c3719f8b1
satozaki-n <hornon.sato31@gmail.com>
2020-09-17 12:18:10 +0900

    Revert "商品の一覧表示機能の実装"
    This reverts commit e7bf17352679157da664ab14ddc107ac992e1227.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,7 +40,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 
 end
