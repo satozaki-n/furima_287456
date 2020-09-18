a8c3a71647421c47fa74df6a17369c1445d0a65a
satozaki-n <hornon.sato31@gmail.com>
2020-09-18 16:03:58 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,6 +32,13 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @items = Item.find(params[:id])
+    @items.destroy
+    redirect_to root_path
+  end
+  
   private
 
   def item_params
