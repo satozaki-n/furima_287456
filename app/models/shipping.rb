2ae31f07aa114e33b14f8c09902d04ef3933ab18
satozaki-n <hornon.sato31@gmail.com>
2020-09-23 20:54:34 +0900

    バリデーションの設定

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,3 +1,10 @@
 class Shipping < ApplicationRecord
   belongs_to :purcases
+  with_options presence: true do 
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :addresses
+    validates :phone_number
+  end
 end
