71e9795947dd33aa374ea8c8c54f7e9062fc1d16
satozaki-n <hornon.sato31@gmail.com>
2020-09-23 16:44:53 +0900

    購入機能の実装

---
+++ app/javascript/card.js
@@ -0,0 +1,34 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_e60eabd91561435dd8e2df90");
+  const form = document.getElementById("credit-card-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+ 
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+ 
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("credit-card-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+ 
+      document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").reset();
+    });
+  });
+ };
+ 
+ window.addEventListener("load", pay);
