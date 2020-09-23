caabbfb254bd7e1b619db9cf5e00d810a1a688dc
satozaki-n <hornon.sato31@gmail.com>
2020-09-23 17:41:15 +0900

    shippingテーブルの作成

--- db/migrate/20200923074519_create_shippings.rb
+++ db/migrate/20200923074519_create_shippings.rb
@@ -1,6 +1,13 @@
 class CreateShippings < ActiveRecord::Migration[6.0]
   def change
     create_table :shippings do |t|
+      t.string      :postal_code,     null: false
+      t.integer     :prefecture_id,   null: false
+      t.string      :city,            null: false
+      t.string      :addresses,       null: false
+      t.string      :building
+      t.string      :phone_number,    null: false
+      t.references  :purchases,       null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_043128) do
+ActiveRecord::Schema.define(version: 2020_09_23_074519) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,19 @@ ActiveRecord::Schema.define(version: 2020_09_23_043128) do
     t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
+  create_table "shippings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "addresses", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.bigint "purchases_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchases_id"], name: "index_shippings_on_purchases_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -79,4 +92,5 @@ ActiveRecord::Schema.define(version: 2020_09_23_043128) do
   add_foreign_key "items", "users"
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
+  add_foreign_key "shippings", "purchases", column: "purchases_id"
 end
