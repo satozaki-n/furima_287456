76dcb523891fc59b257da2a02c14e9199e3f18ef
satozaki-n <hornon.sato31@gmail.com>
2020-09-23 19:18:08 +0900

    shippingテーブルへの保存の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
 
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
   def create
-    @order = Purcases.new(price: order_params[:price])
+    @order = Shipping.new(price: order_params[:id])
     if @order.valid?
       pay_item
       @order.save
@@ -12,17 +16,20 @@ class OrdersController < ApplicationController
   end
 
   private
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
 
   def order_params
-    params.permit(:price, :token)
+    params.permit(:pistalcode, :prefecture_id, :city, :addresses, :building, :phone_number)
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_b6d8402ca02c3529c80a7fde"  # PAY.JPテスト秘密鍵
+    Payjp.api_key = "sk_test_b6d8402ca02c3529c80a7fde"
     Payjp::Charge.create(
       amount: order_params[:price],  # 商品の値段
       card: order_params[:token],    # カードトークン
-      currency:'jpy'                 # 通貨の種類(日本円)
+      currency:'jpy'
     )
   end
 
