eb1985641325e89d78d2004f6e8184548ae76ca4
satozaki-n <hornon.sato31@gmail.com>
2020-09-24 12:26:55 +0900

    モデルの編集

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,10 +1,3 @@
 class Shipping < ApplicationRecord
-  belongs_to :purcases
-  with_options presence: true do 
-    validates :postal_code
-    validates :prefecture_id
-    validates :city
-    validates :addresses
-    validates :phone_number
-  end
+  belongs_to :purchase
 end
