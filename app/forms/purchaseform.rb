dd16267e135c672e6fbeaeb31d126d0505b319a6
satozaki-n <hornon.sato31@gmail.com>
2020-09-24 17:05:01 +0900

    バリデーションの修正

--- app/forms/purchaseform.rb
+++ app/forms/purchaseform.rb
@@ -7,8 +7,10 @@ class Purchaseform
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :addresses
-    validates :phone_number, format: {with: /\A[0-9]+\z/, maxlength: "11"}
+    validates :phone_number, format: {with: /\A[0-9]+\z/, message: "is invalid"}
+    validates :phone_number, length: {maximum: 11, message: "is invalid" }
   end
+
 
   def save
     @purchase = Purchase.create(user_id: user_id, item_id: item_id)
