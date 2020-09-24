440240f861c27b27354fe9f1101501c3868d9c7f
satozaki-n <hornon.sato31@gmail.com>
2020-09-24 12:28:05 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,34 +2,32 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @order = Shipping.new
+    @order = Purchaseform.new
   end
 
   def create
-    @order = Shipping.new(order_params)
+    @item = Item.find(params[:item_id])
+    @order = Purchaseform.new(order_params)
+    
     if @order.valid?
       pay_item
       @order.save
       return redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
-  def item_params
-    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
-  end
 
   def order_params
-    params.require(:shipping).permit(:taken, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+    params.permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id, :token).merge(user_id: current_user.id)
   end
 
   def pay_item
     Payjp.api_key = "sk_test_b6d8402ca02c3529c80a7fde"
     Payjp::Charge.create(
-      amount: item_params[:price],
+      amount: @item.price,
       card: order_params[:token],
       currency:'jpy'
     )
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,7 @@
 const pay = () => {
   Payjp.setPublicKey("pk_test_e60eabd91561435dd8e2df90");
-  const form = document.getElementById("credit-card-form");
+  const form = document.getElementById("charge-form");
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
  
@@ -8,22 +9,22 @@ const pay = () => {
     const formData = new FormData(formResult);
  
     const card = {
-      number: formData.get("shipping[number]"),
-      exp_month: formData.get("shipping[exp_month]"),
-      exp_year: `20${formData.get("shipping[exp_year]")}`,
-      cvc: formData.get("shipping[cvc]"),
+      number: formData.get("number"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+      cvc: formData.get("cvc"),
     };
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        const renderDom = document.getElementById("credit-card-form");
+        const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      document.getElementById("card-number").removeAttribute("id");
-      document.getElementById("card-exp-month").removeAttribute("id");
-      document.getElementById("card-exp-year").removeAttribute("id");
-      document.getElementById("card-cvc").removeAttribute("id");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
  
       document.getElementById("charge-form").submit();
       document.getElementById("charge-form").reset();
